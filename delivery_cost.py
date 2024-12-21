62962a483e914ff4d37a78957c92b2e5bb416388
rishavdevtiwari <rishavtiwari.ai@gmail.com>
2024-12-21 15:06:14 +0545

    re-arrangement of code

--- delivery_cost.py
+++ delivery_cost.py
@@ -3,18 +3,17 @@
 weight=int(input("Enter the weight of the package(kgs) : "))
 urgency=input("is the delivery urgent? (Yes/No) : ")
 delivery_cost=0
-if urgency.lower()=="yes":
-    delivery_cost+=5
-elif urgency.lower()=="no":
-    delivery_cost+=0
-else:
-    print("Invalid input")
-    
 if weight<5:
     delivery_cost+=5
     print(f"The delivery cost is {delivery_cost}")
 elif 5<=weight<=20:
     delivery_cost+=10
+    if urgency.lower()=="yes":
+        delivery_cost+=5
+    elif urgency.lower()=="no":
+        delivery_cost+=0
+    else:
+        print("Invalid input")
     print(f"The delivery cost is {delivery_cost}")
 else:
     delivery_cost+=20
