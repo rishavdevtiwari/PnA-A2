261b48e63d59589ab5d298e97fee4297f4b63ee1
rishavdevtiwari <rishavtiwari.ai@gmail.com>
2024-12-21 15:18:02 +0545

    adding interest rate calculations

--- loan.py
+++ loan.py
@@ -6,13 +6,20 @@
 #  If the credit score is below 600, reject the loan.
 #  If the income is below 50,000, reject the loan
 
-income=int(input("Enter your income amount"))
-credit_score=int(input("Enter your credit score"))
+income=int(input("Enter your income amount : "))
+credit_score=int(input("Enter your credit score : "))
+rate=int(input("Enter the current interest rate : "))
+interest=0
+interest_amt=0
 if income>=50000:
     if credit_score>700:
         print("Loan approved")
     elif 600<=credit_score<=700:
         print("Your loan will be approved with an increased interest rate!")
+        interest_amt=700-credit_score
+        interest=((interest_amt/700)*100)/7
+        print(f"Your interest rate has been increased by {interest}")
+        print(f"Your interest rate total is {rate+interest}")
     elif credit_score<600:
         print("Loan rejected!!!")
 else:
